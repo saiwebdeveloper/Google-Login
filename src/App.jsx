cb3f90b5bc4739993423d4b1ec0be1febf203e8c
saiwebdeveloper <gopasaiv@gmail.com>
2026-01-24 20:38:36 +0530

    update expenses logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
 import { Routes, Route } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 import Login from "./Login";
 import Home from "./Home";
 import ProtectedRoute from "./ProtectedRoute";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route
-        path="/home"
-        element={
-          <ProtectedRoute>
-            <Home />
-          </ProtectedRoute>
-        }
-      />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+
+      {/* Vercel Speed Insights – global */}
+      <SpeedInsights />
+    </>
   );
 }
 
 export default App;
+
